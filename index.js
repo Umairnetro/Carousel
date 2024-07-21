745366716ea759dc576447bebb23dbf0ed7e5cdb
Umair Ahmed <umairjdu@gmail.com>
2024-07-22 03:09:35 +0500

    update

--- index.js
+++ index.js
@@ -7,19 +7,31 @@ let displayImages = [
   "https://images.unsplash.com/photo-1491156855053-9cdff72c7f85?q=80&w=1828&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
 ];
 
+let placeNames = [
+  "Byodo-In Temple",
+  "Catedral de la Inmaculada Concepción",
+  "Germany Small Town",
+  "Natural History Museum",
+];
+
 const image = document.querySelector("#image");
 const previousBtn = document.querySelector(".previous-btn");
 const nextBtn = document.querySelector(".next-btn");
+const heading = document.querySelector("h1");
 
 let currentIndex = 0;
 
 image.setAttribute("src", displayImages[0]);
+heading.innerHTML = placeNames[0];
 function showNext() {
   currentIndex = (currentIndex + 1) % displayImages.length;
   image.classList.add("fade");
+  heading.classList.add("heading-fade");
   setTimeout(() => {
-    image.setAttribute("src", displayImages[currentIndex]);
-    image.classList.remove("fade");
+      image.setAttribute("src", displayImages[currentIndex]);
+      image.classList.remove("fade");
+      heading.classList.remove("heading-fade");
+      heading.innerHTML = placeNames[currentIndex];
   }, 400);
 }
 
@@ -27,9 +39,12 @@ function showprevious() {
   currentIndex =
     (currentIndex - 1 + displayImages.length) % displayImages.length;
   image.classList.add("fade");
+  heading.classList.add("heading-fade");
   setTimeout(() => {
     image.setAttribute("src", displayImages[currentIndex]);
     image.classList.remove("fade");
+    heading.innerHTML = placeNames[currentIndex];
+    heading.classList.remove("heading-fade");
   }, 600);
 }
 
